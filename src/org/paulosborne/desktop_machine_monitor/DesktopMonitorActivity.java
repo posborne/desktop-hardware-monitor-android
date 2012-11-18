9c9ef15a833b5e63c9f6111582e8b62d64a951a5
Paul Osborne <osbpau@gmail.com>
2012-11-17 21:04:55 -0600

    Add additional UI elements and refresh in background on timer

--- src/org/paulosborne/desktop_machine_monitor/DesktopMonitorActivity.java
+++ src/org/paulosborne/desktop_machine_monitor/DesktopMonitorActivity.java
@@ -3,6 +3,10 @@ package org.paulosborne.desktop_machine_monitor;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -12,20 +16,32 @@ import org.json.JSONObject;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.AsyncTask;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB) public class DesktopMonitorActivity extends Activity {
 
+	/* Constants */
+	private static final int THREADPOOL_THREADS = 5;
+	private static final int CHECK_SECONDS = 5;
+	private static final String STATSDUMP_URL = "http://192.168.1.50:8080/api/statsdump";
+
+	private ScheduledExecutorService scheduledTaskExecutor;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desktop_monitor);
+        scheduledTaskExecutor = Executors.newScheduledThreadPool(THREADPOOL_THREADS);
+        scheduledTaskExecutor.scheduleAtFixedRate(new Runnable() {
+        	public void run() {
+        		updateStatisticsAndView();
+        	}
+        }, 0, CHECK_SECONDS, TimeUnit.SECONDS);
     }
 
     @Override
@@ -35,51 +51,53 @@ import android.widget.TextView;
         return true;
     }
 
-    public void updateDisplay(View view) {
-    	UpdateDesktopStaticsTask updateDesktopStatisticsTask = new UpdateDesktopStaticsTask();
-    	updateDesktopStatisticsTask.execute(new String[] { "http://192.168.1.50:8080/api/statsdump" });
-    }
-
-    private class UpdateDesktopStaticsTask extends AsyncTask<String, Void, JSONObject> {
-    	@Override
-    	protected JSONObject doInBackground(String... urls) {
-    		for (String url: urls) {
-    			DefaultHttpClient client = new DefaultHttpClient();
-    			HttpGet httpGet = new HttpGet(url);
-    			try {
-    				HttpResponse response = client.execute(httpGet);
-    				InputStream content = response.getEntity().getContent();
-    				BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
-    				StringBuilder sb = new StringBuilder();
-    				String line;
-    				while ((line = buffer.readLine()) != null) {
-    					sb.append(line + "\n");
-    				}
-    				content.close();
-    				JSONObject jsonObject = new JSONObject(sb.toString());
-    				return jsonObject;
-    			} catch (Exception e) {
-    				e.printStackTrace();
-    			}
-    		}
-    		return null;
-    	}
-    	
-    	@Override
-    	protected void onPostExecute(JSONObject jsonObject) {
-    		System.out.println(jsonObject);
-    		TextView cpuUsageTextView = (TextView)findViewById(R.id.cpuUsage);
-    		ProgressBar cpuUsageProgressBar = (ProgressBar)findViewById(R.id.cpuProgressBar);
-    		try {
-				int cpuLoadPercent = jsonObject.getInt("cpu_load_percent");
-	    		cpuUsageTextView.setText("" + cpuLoadPercent);
-	    		cpuUsageProgressBar.setProgress(cpuLoadPercent);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+    /*
+     * Go to the REST webserver and grab the latest statistics and update the UI
+     */
+    public void updateStatisticsAndView() {
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(STATSDUMP_URL);
+		try {
+			HttpResponse response = client.execute(httpGet);
+			InputStream content = response.getEntity().getContent();
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = buffer.readLine()) != null) {
+				sb.append(line + "\n");
 			}
-    	}
+			content.close();
+			final JSONObject jsonObject = new JSONObject(sb.toString());
+			runOnUiThread(new Runnable() {
+				public void run() {
+		    		TextView cpuUsageTextView = (TextView)findViewById(R.id.cpuUsage);
+		    		ProgressBar cpuUsageProgressBar = (ProgressBar)findViewById(R.id.cpuProgressBar);
+		    		TextView ramFreeTextView = (TextView)findViewById(R.id.ramFreeTextView);
+		    		ProgressBar ramFreeProgressBar = (ProgressBar)findViewById(R.id.ramFreeProgressBar);
+		    		TextView cpuTemperatureTextView = (TextView)findViewById(R.id.cpuTemperatureTextView);
+		    		try {
+						int cpuLoadPercent = jsonObject.getInt("cpu_load_percent");
+						double cpuTempCelsius = jsonObject.getDouble("cpu_temp_celsius");
+						long totalMemory = jsonObject.getLong("total_memory");
+						long freeMemory = jsonObject.getLong("free_memory");
+			    		double ramFreePercent = ((double)freeMemory / totalMemory);
+						cpuUsageTextView.setText("" + cpuLoadPercent);
+			    		cpuUsageProgressBar.setProgress(cpuLoadPercent);
+			    		DecimalFormat df = new DecimalFormat("#.##");
+			    		ramFreeTextView.setText("" + df.format(freeMemory / 1024.0 / 1024.0) + " MB (Total: " + df.format(totalMemory / 1024.0 / 1024.0) + ")");
+			    		ramFreeProgressBar.setProgress((int)(100 * ramFreePercent));
+			    		cpuTemperatureTextView.setText("CPU Temp (Celsius): " + cpuTempCelsius);
+			    		cpuTemperatureTextView.setBackgroundColor(Color.GREEN);
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
     }
-    
     
 }
